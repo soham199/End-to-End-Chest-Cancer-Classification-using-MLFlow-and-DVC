83fb5574c1bac5777bdca32bbb3a0fb8da47d017
soham <sohampatra1234@gmail.com>
2024-03-27 03:01:24 +0530

    Model Training completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from CNNCalssifier import logger
 from CNNCalssifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from CNNCalssifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from CNNCalssifier.pipeline.stage_03_model_trainer import Model_trainer
 
 
 STAGE_NAME="DATA INGESTION PIPELINE"
@@ -24,6 +25,19 @@ if __name__ == '__main__':
         logger.info(f"*******************")
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    STAGE_NAME="Model Training"
+    
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = Model_trainer()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
---
+++ src/CNNCalssifier/pipeline/stage_03_model_trainer.py
@@ -0,0 +1,34 @@
+from cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier.components.prepare_base_model import PrepareBaseModel
+from cnnClassifier import logger
+from CNNCalssifier.components.model_trainer import Training
+
+
+
+STAGE_NAME = "Model Trainer"
+
+
+class Model_trainer:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train()
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = Model_trainer()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
