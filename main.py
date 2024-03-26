e798981742aaf0518070f90e1bab33faae5bf544
soham <sohampatra1234@gmail.com>
2024-03-27 01:25:31 +0530

    Prepare_base_model_added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from CNNCalssifier import logger
 from CNNCalssifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from CNNCalssifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME="DATA INGESTION PIPELINE"
@@ -8,6 +9,21 @@ if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    
+STAGE_NAME="PREPARE BASE MODEL"
+    
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
